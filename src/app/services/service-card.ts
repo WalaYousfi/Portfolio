31fb9ee1107ce7198992704d6386dcf3a83e230d
Wala Yousfi <yousfiwala9@gmail.com>
2024-08-27 22:27:07 +0100

    service

---
+++ src/app/services/service-card.ts
@@ -0,0 +1,5 @@
+export interface ServiceCard {
+    title: string;
+    icon: string;
+    description: string
+}
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
-
+import { ServiceCard } from './service-card';
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrl: './services.component.scss'
+  styleUrl: './services.component.scss',
 })
 export class ServicesComponent {
-
+  services: ServiceCard[] = [
+    {
+      title: 'title',
+      icon: 'icon',
+      description:
+        "    Explore my portfolio to see how I bring ideas to life through fullstack development. Let's collaborate to build something remarkable together.",
+    },
+    {
+      title: 'title',
+      icon: 'icon',
+      description:
+        "    Explore my portfolio to see how I bring ideas to life through fullstack development. Let's collaborate to build something remarkable together.",
+    },
+    {
+      title: 'title',
+      icon: 'icon',
+      description:
+        "    Explore my portfolio to see how I bring ideas to life through fullstack development. Let's collaborate to build something remarkable together.",
+    },
+    {
+      title: 'title',
+      icon: 'icon',
+      description:
+        "    Explore my portfolio to see how I bring ideas to life through fullstack development. Let's collaborate to build something remarkable together.",
+    },
+    {
+      title: 'title',
+      icon: 'icon',
+      description:
+        "    Explore my portfolio to see how I bring ideas to life through fullstack development. Let's collaborate to build something remarkable together.",
+    },
+    {
+      title: 'title',
+      icon: 'icon',
+      description:
+        "    Explore my portfolio to see how I bring ideas to life through fullstack development. Let's collaborate to build something remarkable together.",
+    },
+  ];
 }
